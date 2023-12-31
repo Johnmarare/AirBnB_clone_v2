7754919150558676b7e44f11388972e6b3bb8542
Johnmarare <mararejohn@gmail.com>
2023-12-31 13:44:24 +0300

    only if number

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """display Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def sec_index():
+    """display HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_index(text):
+    """C is Fun"""
+    text = text.replace('_', ' ')
+    return "C {}".format(text)
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_index(text):
+    """Python is Fun"""
+    text = text.replace('_', ' ')
+    return "Python {}".format(text)
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def n_index(n):
+    """n is a number"""
+    if n.isnumeric():
+        return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
